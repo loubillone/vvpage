0fad389bc9532a34f50d283a17dab00f3b9b6643
Lourdes Billone <lou.billone@gmail.com>
2024-11-02 22:54:10 -0300

    acomodo elementos del nav

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -3,31 +3,43 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import "../css/home.css";
+import vvIndex from "../assets/img/index/vvIndex.png";
 
 const Navegador = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary ">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home" className="vvIndex">
+            <img src={vvIndex} alt="victoria villarruel" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+              <Nav.Link href="#home" className="nav-items">
+                Inicio
+              </Nav.Link>
+              <NavDropdown
+                title="Biografía"
+                id="basic-nav-dropdown"
+                className="nav-items"
+              >
+                <NavDropdown.Item href="#action/3.1">CELTYV</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Malvinas</NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link href="#link" className="nav-items">
+                Trayectoria
+              </Nav.Link>
+              <Nav.Link href="#link" className="nav-items">
+                Noticias
+              </Nav.Link>
+              <Nav.Link href="#link" className="nav-items">
+                Galeria
+              </Nav.Link>
+              <Nav.Link href="#link" className="nav-items">
+                Contacto
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
