35ca8fe503ede9f5f89fadca94e341ef056d5751
Lourdes Billone <lou.billone@gmail.com>
2024-11-04 03:56:51 -0300

    col de cards para mobile y escritorio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import FlyerHome from "../components/FlyerHome";
+import UltimasNoticias from "../components/UltimasNoticias";
 
 const Home = () => {
   return (
     <div>
       <FlyerHome />
+
+      <UltimasNoticias />
     </div>
   );
 };
