a36a9c417fe80c4725adc452fe498052ba68d3f8
Lourdes Billone <lou.billone@gmail.com>
2025-01-02 00:12:26 -0300

    foto con box detalle ok en escritorio

--- src/components/FotosIndex.jsx
+++ src/components/FotosIndex.jsx
@@ -23,11 +23,45 @@ const FotosIndex = () => {
         </div>
 
         <div className="box-indexJM">
-          <h2>JAVIER MILEI</h2>
+          <div className="texto-box-index">
+            <h2 className="titulo-box">Javier </h2>
+            <h2 className="titulo-box-dos">Milei</h2>
+            <h6 className="subtitulo-box">Presidente de la Nación</h6>
+            <h5 className="titulo-parrafo-box">
+              Por la Verdad, la Justicia y la Libertad
+            </h5>
+            <p className="texto-parrafo-box">
+              La verdad y la justicia son pilares indispensables para construir
+              una Argentina fuerte y unida. Mi compromiso es con todos los
+              ciudadanos, para asegurar que sus derechos sean protegidos y que
+              la memoria de nuestro país se preserve completa y sin
+              distorsiones. Lucharé por una nación donde la libertad individual
+              sea el motor del progreso y donde cada argentino tenga la
+              oportunidad de prosperar. Juntos, podemos transformar nuestra
+              patria en un lugar de paz, reconciliación y oportunidades para
+              todos.
+            </p>
+          </div>
         </div>
 
         <div className="box-indexVV">
-          <h1>VICTORIA VILLARRUEL</h1>
+          <div className="texto-box-index">
+            <h1 className="titulo-box">Victoria </h1>
+            <h2 className="titulo-box-dos">Villarruel</h2>
+            <h6 className="subtitulo-box">Vicepresidente de la Nación</h6>
+            <h5 className="titulo-parrafo-box">
+              Unidos por la Libertad y la Justicia
+            </h5>
+            <p className="texto-parrafo-box">
+              Como defensora de los valores de justicia y libertad, me
+              enorgullece trabajar junto al Presidente Javier Milei en la
+              transformación de nuestra nación. Juntos, estamos comprometidos en
+              asegurar un futuro próspero y seguro para todos los argentinos,
+              donde la transparencia y el respeto por los derechos humanos sean
+              pilares fundamentales. Apoyemos esta misión para construir un país
+              más justo y libre para las generaciones venideras.
+            </p>
+          </div>
         </div>
       </div>
     </div>
