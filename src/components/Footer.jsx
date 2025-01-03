d35c5971520dbed550e7bc796b531cb75a2d2359
Lourdes Billone <lou.billone@gmail.com>
2025-01-03 18:10:18 -0300

    soluciono overflow de fotos del index con la transicion

---
+++ src/components/Footer.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import "../css/footer.css";
+import facebook from "../assets/img/footer/facebook.png";
+import instagram from "../assets/img/footer/instagram.png";
+import x from "../assets/img/footer/x.png";
+import youtube from "../assets/img/footer/youtube.png";
+import logoVV from "../assets/img/footer/logoVV.png";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="container-fluid container-footer">
+        <div className="row">
+          <div className="col columna-iconos-footer">
+            <div>
+              <img
+                src={facebook}
+                alt="icono facebook"
+                className="iconos-footer"
+              />
+            </div>
+            <div>
+              <img src={x} alt="icono x" className="iconos-footer" />
+            </div>
+            <div>
+              <img
+                src={instagram}
+                alt="icono instragram"
+                className="iconos-footer"
+              />
+            </div>
+            <div>
+              <img
+                src={youtube}
+                alt="icono youtube"
+                className="iconos-footer"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col columna-logoVV-footer">
+            <img src={logoVV} alt="logo vv" className="logoVV-footer" />
+          </div>
+        </div>
+
+        <div className="row fila-container-texto-footer">
+          <div className="col">
+            <span className="titulo-enlaces-footer">Navegación</span>
+
+            <div className="col enlaces-footer">
+              <span>Inicio</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Biografía</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Trayectoria</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Noticias</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Galería</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Contacto</span>
+            </div>
+          </div>
+          <div className="col">
+            <div>
+              <span className="titulo-enlaces-footer">
+                Información y Contacto
+              </span>
+            </div>
+
+            <div className="col enlaces-footer">
+              <span>Contacto con el equipo</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Contáctenos</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Notas legales</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Protección de Datos</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Transparencia y acceso a la información</span>
+            </div>
+            <div className="col enlaces-footer">
+              <span>Reglamento Interno</span>
+            </div>
+          </div>
+          <div className="col">
+            <div>
+              <span className="titulo-enlaces-footer">Redes Sociales</span>
+            </div>
+            <div>
+              <div className="col enlaces-footer">
+                <span>Twitter</span>
+              </div>
+              <div className="col enlaces-footer">
+                <span>Facebook</span>
+              </div>
+              <div className="col enlaces-footer">
+                <span>Instagram</span>
+              </div>
+              <div className="col enlaces-footer">
+                <span>TikTok</span>
+              </div>
+              <div className="col enlaces-footer">
+                <span>Youtube</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FlyerHome from "../components/FlyerHome";
 import UltimasNoticias from "../components/UltimasNoticias";
 import FotosIndex from "../components/FotosIndex";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -11,6 +12,8 @@ const Home = () => {
       <UltimasNoticias />
 
       <FotosIndex />
+
+      <Footer />
     </div>
   );
 };
