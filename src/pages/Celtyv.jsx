f65c86735978fb17b3c275a2d9e9cd6b871e0799
Lourdes Billone <lou.billone@gmail.com>
2025-01-04 15:34:51 -0300

    posicionamiento de box en imagenes

---
+++ src/pages/Celtyv.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Celtyv = () => {
+  return (
+    <div>
+      <h2>celtyv</h2>
+    </div>
+  );
+};
+
+export default Celtyv;
---
+++ src/pages/Malvinas.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Malvinas = () => {
+  return (
+    <div>
+      <h2>Malvinas</h2>
+    </div>
+  );
+};
+
+export default Malvinas;
