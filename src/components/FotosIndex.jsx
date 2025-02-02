9745f8aa908c8e674f977beef3b7fbb8d5711fbb
Lourdes Billone <lou.billone@gmail.com>
2025-02-02 16:33:09 -0300

    index version 480px ok

--- src/components/FotosIndex.jsx
+++ src/components/FotosIndex.jsx
@@ -9,30 +9,79 @@ const FotosIndex = () => {
   const toggleBox = (box) => {
     setActiveBox(activeBox === box ? null : box);
   };
+
   return (
     <div>
       <div className="container-fluid container-fotos-index">
         <div className="row fila-fotos-index">
-          <img
-            onClick={() => toggleBox("boxJM")}
-            className="imagenIndex"
-            src={vvjm}
-            alt="Victoria Villarruel y Javier Milei en la asunción presidencial"
-          />
+          <div className="contenedor-imagen">
+            <img
+              onClick={() => toggleBox("boxJM")}
+              className="imagenIndex"
+              src={vvjm}
+              alt="Victoria Villarruel y Javier Milei en la asunción presidencial"
+            />
+            <div
+              className="texto-sobre-imagen"
+              onClick={() => toggleBox("boxJM")}
+            >
+              <h2 className="titulo-sobre-imagen">Javier Milei</h2>
+              <p className="subtitulo-sobre-imagen">Presidente de la Nación</p>
+            </div>
+          </div>
 
-          <img
-            onClick={() => toggleBox("boxVV")}
-            className="imagenIndex"
-            src={vvgala}
-            alt="Victoria Villarruel en la gala del Teatro Colón"
-          />
+          <div className="contenedor-imagen">
+            <img
+              onClick={() => toggleBox("boxVV")}
+              className="imagenIndex"
+              src={vvgala}
+              alt="Victoria Villarruel en la gala del Teatro Colón"
+            />
+            <div
+              className="texto-sobre-imagen"
+              onClick={() => toggleBox("boxVV")}
+            >
+              <h2 className="titulo-sobre-imagen">Victoria Villarruel</h2>
+              <p className="subtitulo-sobre-imagen">
+                Vicepresidente de la Nación
+              </p>
+            </div>
+          </div>
         </div>
 
+        {/* Fondo oscuro (overlay) */}
+        <div
+          className={`overlay ${activeBox ? "active" : ""}`}
+          onClick={() => toggleBox(null)}
+        ></div>
+
+        {/* Box para Javier Milei */}
         <div className={`box-indexJM ${activeBox === "boxJM" ? "active" : ""}`}>
-          <div className="texto-box-index ">
+          <div className="texto-box-index">
             <h2 className="titulo-box">Javier</h2>
             <h2 className="titulo-box-dos">Milei</h2>
             <h6 className="subtitulo-box">Presidente de la Nación</h6>
+            <h5 className="titulo-parrafo-box">
+              Unidos por la Libertad y la Justicia
+            </h5>
+            <p className="texto-parrafo-box">
+              Como defensora de los valores de justicia y libertad, me
+              enorgullece trabajar junto al Presidente Javier Milei en la
+              transformación de nuestra nación. Juntos, estamos comprometidos en
+              asegurar un futuro próspero y seguro para todos los argentinos,
+              donde la transparencia y el respeto por los derechos humanos sean
+              pilares fundamentales. Apoyemos esta misión para construir un país
+              más justo y libre para las generaciones venideras.
+            </p>
+          </div>
+        </div>
+
+        {/* Box para Victoria Villarruel */}
+        <div className={`box-indexVV ${activeBox === "boxVV" ? "active" : ""}`}>
+          <div className="texto-box-index">
+            <h1 className="titulo-box">Victoria</h1>
+            <h2 className="titulo-box-dos">Villarruel</h2>
+            <h6 className="subtitulo-box">Vicepresidente de la Nación</h6>
             <h5 className="titulo-parrafo-box">
               Por la Verdad, la Justicia y la Libertad
             </h5>
@@ -46,26 +95,6 @@ const FotosIndex = () => {
               oportunidad de prosperar. Juntos, podemos transformar nuestra
               patria en un lugar de paz, reconciliación y oportunidades para
               todos.
-            </p>
-          </div>
-        </div>
-
-        <div className={`box-indexVV ${activeBox === "boxVV" ? "active" : ""}`}>
-          <div className="texto-box-index">
-            <h1 className="titulo-box">Victoria</h1>
-            <h2 className="titulo-box-dos">Villarruel</h2>
-            <h6 className="subtitulo-box">Vicepresidente de la Nación</h6>
-            <h5 className="titulo-parrafo-box">
-              Unidos por la Libertad y la Justicia
-            </h5>
-            <p className="texto-parrafo-box">
-              Como defensora de los valores de justicia y libertad, me
-              enorgullece trabajar junto al Presidente Javier Milei en la
-              transformación de nuestra nación. Juntos, estamos comprometidos en
-              asegurar un futuro próspero y seguro para todos los argentinos,
-              donde la transparencia y el respeto por los derechos humanos sean
-              pilares fundamentales. Apoyemos esta misión para construir un país
-              más justo y libre para las generaciones venideras.
             </p>
           </div>
         </div>
