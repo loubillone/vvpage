f285fc6afe18000b7a3e11f5b2a03ad9ae9fa377
Lourdes Billone <lou.billone@gmail.com>
2025-02-19 03:28:39 -0300

    estructura de fotos y textos

--- src/components/Celtyv.jsx
+++ src/components/Celtyv.jsx
@@ -21,6 +21,213 @@ const Celtyv = () => {
             </span>
           </div>
         </div>
+
+        <div className="row">
+          <div className="foto-manos-celtyv">
+            <img src={manos} alt="mural con marcas de manos en pintura" />
+          </div>
+        </div>
+
+        <div className="row container-celtyvInfo">
+          <div className="col">
+            <h3 className="titulo-seccion-celtyv">¿Qué es CELTyV?</h3>
+            <p className="parrafo-seccion-celtyv">
+              El Centro de Estudios Legales sobre el Terrorismo y sus Víctimas
+              (CELTyV) es una Asociación Civil fundada en 2006. Esta
+              organización no gubernamental se dedica a la asistencia de las
+              víctimas del terrorismo en Argentina. CELTyV trabaja tanto a nivel
+              nacional como internacional, con el propósito de visibilizar a las
+              víctimas, luchar por el reconocimiento de sus derechos y
+              contribuir a la verdad histórica.
+            </p>
+
+            <h4 className="subtitulo-seccion-celtyv">Objetivos y Trabajo</h4>
+            <p className="parrafo-seccion-celtyv">
+              Las víctimas inocentes de la violencia armada ejercida por las
+              organizaciones guerrilleras durante la guerra revolucionaria en
+              Argentina no han recibido justicia, verdad, reparación ni paz. En
+              cambio, han sido sometidas a un deliberado olvido, un cómplice
+              silencio y un injustificable abandono. CELTyV busca cambiar esta
+              situación a través de su labor diaria, enfrentando la parcialidad
+              y desamparo en que los gobiernos han dejado a estas víctimas. El
+              Estado argentino, al no respetar los tratados vigentes de derechos
+              humanos y proteger la impunidad de los perpetradores, incurre en
+              responsabilidad internacional. Desde su creación, CELTyV ha
+              trabajado en la identificación y documentación de las víctimas, la
+              investigación y elaboración de artículos doctrinales sobre la
+              problemática del terrorismo y la difusión de la verdadera historia
+              de la agresión armada de las organizaciones terroristas contra la
+              población civil no combatiente.
+            </p>
+
+            <h4 className="subtitulo-seccion-celtyv">Autoridades de CELTyV</h4>
+
+            <div className="seccion-autoridades-celtyv">
+              <ul>
+                <li>Presidente: Victoria Villarruel – Abogada</li>
+                <li>
+                  Vicepresidente: Isaac Barrios – Padre de un niño asesinado por
+                  Montoneros
+                </li>
+                <li>
+                  Secretario: Sergio Renó – Hijo de un policía asesinado por
+                  Montoneros Vocales:
+                </li>
+                <li>
+                  Arturo Larrabure – Hijo de un militar secuestrado y asesinado
+                  por el ERP
+                </li>
+                <li>Javier Vigo Leguizamón – Abogado especialista en DDHH</li>
+                <li>
+                  Alicia Longinotti – Hija y hermana de dos víctimas asesinadas
+                  por OCPO
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="imagen-victimas-celtyv">
+            <img src={victimas} alt="primer niño asesinado por el terrorismo" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <h4>Las Víctimas</h4>
+            <p>
+              CELTyV se centra en la situación de las víctimas de la violencia
+              armada ejercida por organizaciones guerrilleras como Montoneros y
+              ERP. Estas víctimas incluyen civiles y no combatientes que
+              sufrieron ataques, secuestros y asesinatos. La organización se
+              dedica a documentar sus historias, proporcionando asistencia legal
+              y emocional, y promoviendo el reconocimiento de sus derechos. La
+              labor de CELTyV busca asegurar que estas víctimas obtengan
+              justicia y se reconozca el sufrimiento que han soportado.
+            </p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="imagen-vicky-celtyv">
+            <img src={vvCeltyv} alt="victoria firmando un libro en el celtyv" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <h3>Victoria Villarruel y su Lucha</h3>
+            <p>
+              Victoria Villarruel, presidenta de CELTyV, es una abogada
+              comprometida con la defensa de los derechos de las víctimas del
+              terrorismo. Su trabajo incluye la publicación de libros y
+              artículos que buscan dar voz a estas víctimas y visibilizar su
+              sufrimiento. Villarruel ha sido una figura clave en la lucha por
+              la verdad histórica y el reconocimiento de los derechos de las
+              víctimas del terrorismo en Argentina.
+            </p>
+
+            <h4>Publicaciones Relevantes</h4>
+
+            <h5>Los Otros Muertos</h5>
+
+            <div>
+              <ul>
+                <li>Fecha de Publicación: 14 de julio de 2021</li>
+                <li>
+                  Descripción: Narra la trágica muerte de niños como Juan y
+                  Andrea en ataques perpetrados por Montoneros.
+                </li>
+              </ul>
+            </div>
+
+            <h5>“Los llaman… jóvenes idealistas” por Victoria Villarruel</h5>
+
+            <div>
+              <ul>
+                <li>Fecha de Publicación: 14 de julio de 2021</li>
+                <li>
+                  Descripción: Narra la trágica muerte de niños como Juan y
+                  Andrea en ataques perpetrados por Montoneros.
+                </li>
+              </ul>
+            </div>
+
+            <h5>“El terrorismo como crimen de lesa humanidad”</h5>
+
+            <div>
+              <ul>
+                <li>Fecha de Publicación: 14 de julio de 2021</li>
+                <li>
+                  Descripción: Narra la trágica muerte de niños como Juan y
+                  Andrea en ataques perpetrados por Montoneros.
+                </li>
+              </ul>
+            </div>
+
+            <h5>“El terrorismo como crimen de lesa humanidad”</h5>
+
+            <div>
+              <ul>
+                <li>Fecha de Publicación: 14 de julio de 2021</li>
+                <li>
+                  Descripción: Narra la trágica muerte de niños como Juan y
+                  Andrea en ataques perpetrados por Montoneros.
+                </li>
+              </ul>
+            </div>
+
+            <h5>“El terrorismo como crimen de lesa humanidad”</h5>
+
+            <div>
+              <ul>
+                <li>Fecha de Publicación: 14 de julio de 2021</li>
+                <li>
+                  Descripción: Narra la trágica muerte de niños como Juan y
+                  Andrea en ataques perpetrados por Montoneros.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div>
+            <img src={logoCeltyv} alt="logo celtyv" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <h4>¿Por qué unirse a CELTyV?</h4>
+            <p>
+              Unirse a CELTyV significa apoyar el reconocimiento de los derechos
+              humanos para las víctimas del terrorismo. La asociación realiza
+              diversas actividades como conferencias, investigaciones
+              históricas, eventos y la publicación de libros y videos. Al
+              convertirse en socio, se contribuye a la lucha por la justicia y
+              la memoria histórica.
+            </p>
+
+            <h5>Categorías de Socios</h5>
+            <div>
+              <ul>
+                <li>Socio Adherente: Cuota mensual de $1000. .</li>
+                <li>Socio Activo: Cuota mensual de $1500.</li>
+                <li> Socio Protector: Cuota mensual de $2000</li>
+              </ul>
+            </div>
+
+            <p>
+              Para más información sobre cómo asociarse, envíe un correo a
+              informaciones@victimasdeargentina.com o visite aquí. Su ayuda es
+              vital para continuar trabajando en la reconstrucción de la
+              historia argentina y el reconocimiento de las víctimas del
+              terrorismo.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
