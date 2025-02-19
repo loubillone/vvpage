558f809118479f22abf437b7b2ca26a0f448c405
Lourdes Billone <lou.billone@gmail.com>
2025-02-19 02:49:46 -0300

    posiciono imagen titulo y texto

--- src/components/Celtyv.jsx
+++ src/components/Celtyv.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import "../css/celtyv.css";
+import logoCeltyv from "../assets/img/biografia/celtyv/logoCeltyv.png";
+import manos from "../assets/img/biografia/celtyv/manos.png";
+import tituloCeltyv from "../assets/img/biografia/celtyv/tituloCeltyv.png";
+import victimas from "../assets/img/biografia/celtyv/victimas.png";
+import vvCeltyv from "../assets/img/biografia/celtyv/vvCeltyv.png";
 
 const Celtyv = () => {
   return (
     <div>
-      <h2>CELTYV PRUEBA</h2>
+      <div className="container">
+        <div className="row fila-titulo-celtyv">
+          <div className="titulo-celtyv">
+            <img src={tituloCeltyv} alt="titulo celtyv" />
+          </div>
+
+          <div className="container-parrafo-celtyv">
+            <span className="texto-titulo-celtyv">
+              Centro de Estudios Legales sobre el Terrorismo y sus Víctimas
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
