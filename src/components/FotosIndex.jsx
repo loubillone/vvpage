4d2a2d8f2294dd49579eafcecace8c3bbc638005
Lourdes Billone <lou.billone@gmail.com>
2025-11-01 18:22:45 -0300

    dejo solo el box de VV

--- src/components/FotosIndex.jsx
+++ src/components/FotosIndex.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import vvgala from "../assets/img/index/vv-gala-index.png";
-import vvjm from "../assets/img/index/vv-jm-index.png";
 import "../css/boxTextos.css";
 
 const FotosIndex = () => {
@@ -36,24 +35,6 @@ const FotosIndex = () => {
     <div>
       <div className="container-fluid container-fotos-index">
         <div className="row fila-fotos-box">
-          {/* Imagen de Javier Milei */}
-          <div
-            className="contenedor-imagen"
-            onMouseEnter={() => activateBox("boxJM")}
-            onClick={() => isMobile && toggleBox("boxJM")}
-          >
-            <img
-              className="imagenBox"
-              src={vvjm}
-              alt="Victoria Villarruel y Javier Milei en la asunción presidencial"
-            />
-            <div className="texto-sobre-imagen">
-              <h2 className="titulo-sobre-imagen">Javier Milei</h2>
-              <p className="subtitulo-sobre-imagen">Presidente de la Nación</p>
-            </div>
-          </div>
-
-          {/* Imagen de Victoria Villarruel */}
           <div
             className="contenedor-imagen"
             onMouseEnter={() => activateBox("boxVV")}
@@ -62,7 +43,7 @@ const FotosIndex = () => {
             <img
               className="imagenBox"
               src={vvgala}
-              alt="Victoria Villarruel en la gala del Teatro Colón"
+              alt="Victoria Villarruel en su llegada a la gala en el teatro Colón"
             />
             <div className="texto-sobre-imagen">
               <h2 className="titulo-sobre-imagen">Victoria Villarruel</h2>
@@ -81,36 +62,11 @@ const FotosIndex = () => {
           ></div>
         )}
 
-        {/* Box para Javier Milei */}
-        <div
-          className={`box-indexJM ${activeBoxes.has("boxJM") ? "active" : ""}`}
-        >
-          <div className="texto-box">
-            <h2 className="titulo-box">Javier</h2>
-            <h2 className="titulo-box-dos">Milei</h2>
-            <h6 className="subtitulo-box">Presidente de la Nación</h6>
-            <h5 className="titulo-parrafo-box">
-              Unidos por la Libertad y la Justicia
-            </h5>
-            <p className="texto-parrafo-box">
-              Junto al Presidente Javier Milei, Victoria Villarruel impulsa una
-              nueva etapa en la historia argentina. Su trabajo refleja la
-              convicción de que el cambio solo es posible a través del esfuerzo,
-              la unidad y el amor por la Patria. Con una mirada puesta en el
-              futuro, promueve políticas que fortalezcan las instituciones,
-              devuelvan la esperanza al pueblo y consoliden una Nación libre,
-              soberana y próspera, capaz de recuperar su grandeza y proyectarse
-              al mundo con orgullo.
-            </p>
-          </div>
-        </div>
-
-        {/* Box para Victoria Villarruel */}
         <div
           className={`box-indexVV ${activeBoxes.has("boxVV") ? "active" : ""}`}
         >
           <div className="texto-box">
-            <h1 className="titulo-box">Victoria</h1>
+            <h2 className="titulo-box">Victoria</h2>
             <h2 className="titulo-box-dos">Villarruel</h2>
             <h6 className="subtitulo-box">Vicepresidente de la Nación</h6>
             <h5 className="titulo-parrafo-box">Por la unidad nacional</h5>
