67d7f044e0951a26599e6c985250d6a341f3a44b
Lourdes Billone <lou.billone@gmail.com>
2026-05-01 22:44:02 -0300

    agrego visita a mendoza

--- src/pages/ProvinciaDetalle.jsx
+++ src/pages/ProvinciaDetalle.jsx
@@ -64,6 +64,26 @@ import mendozaVVTres from "../assets/img/mapaArg/mendoza/mendozaVV3.webp";
 import mendozaVVCuatro from "../assets/img/mapaArg/mendoza/mendozaVV4.webp";
 import mendozaVVCinco from "../assets/img/mapaArg/mendoza/mendozaVV5.webp";
 import mendozaVVSeis from "../assets/img/mapaArg/mendoza/mendozaVV6.webp";
+import mendozaVV2VV from "../assets/img/mapaArg/mendoza/vv-mendoza-coviar.webp";
+import mendozaVV2VVDos from "../assets/img/mapaArg/mendoza/vv-mendoza-coviar2.webp";
+import mendozaVV2VVTres from "../assets/img/mapaArg/mendoza/vv-mendoza-coviar3.webp";
+import mendozaVV2VVCuatro from "../assets/img/mapaArg/mendoza/vv-mendoza-parroquia.webp";
+import mendozaVV2VVCinco from "../assets/img/mapaArg/mendoza/vv-mendoza-parroquia2.webp";
+import mendozaVV2VVSeis from "../assets/img/mapaArg/mendoza/vv-mendoza-parroquia3.webp";
+import mendozaVV2VVSiete from "../assets/img/mapaArg/mendoza/vv-mendoza-parroquia4.webp";
+import mendozaVV2VVOcho from "../assets/img/mapaArg/mendoza/vv-mendoza-parroquia5.webp";
+import mendozaVV2VVNueve from "../assets/img/mapaArg/mendoza/vv-mendoza-parroquia6.webp";
+import mendozaVV2VVDiez from "../assets/img/mapaArg/mendoza/vv-mendoza-prod.webp";
+import mendozaVV2VVOnce from "../assets/img/mapaArg/mendoza/vv-mendoza-prod2.webp";
+import mendozaVV2VVDoce from "../assets/img/mapaArg/mendoza/vv-mendoza-prod3.webp";
+import mendozaVV2VVTrece from "../assets/img/mapaArg/mendoza/vv-mendoza-prod4.webp";
+import mendozaVV2VVCatorce from "../assets/img/mapaArg/mendoza/vv-mendoza-prod5.webp";
+import mendozaVV2VVQuince from "../assets/img/mapaArg/mendoza/vv-mendoza-prod6.webp";
+import mendozaVV2VVDieciseis from "../assets/img/mapaArg/mendoza/vv-mendoza-prod7.webp";
+import mendozaVV2VVDiecisiete from "../assets/img/mapaArg/mendoza/vv-mendoza-prod8.webp";
+import mendozaVV2VVDieciocho from "../assets/img/mapaArg/mendoza/vv-mendoza-prod9.webp";
+import mendozaVV2VVDiecinueve from "../assets/img/mapaArg/mendoza/vv-mendoza-prod10.webp";
+import mendozaVV2VVVeinte from "../assets/img/mapaArg/mendoza/vv-mendoza-prod11.webp";
 import sanLuisVV from "../assets/img/mapaArg/san-luis/sanluisVV.webp";
 import sanLuisVVDos from "../assets/img/mapaArg/san-luis/sanluisVV2.webp";
 import sanLuisVVTres from "../assets/img/mapaArg/san-luis/sanluisVV3.webp";
@@ -1178,6 +1198,224 @@ const ProvinciaDetalle = () => {
             mendozaVVSeis,
           ],
           lugar: "Ciudad de Mendoza",
+        },
+        {
+          fecha: "Marzo de 2026",
+          lugar: "Mendoza",
+          actividades: [
+            {
+              titulo: "Desayuno COVIAR",
+              descripcion: (
+                <>
+                  <p>
+                    La vicepresidente Victoria Villarruel participó del{" "}
+                    <span className="vv-estilo-detalle">
+                      tradicional desayuno de la Corporación Vitivinícola
+                      Argentina (COVIAR)
+                    </span>{" "}
+                    en la provincia de{" "}
+                    <span className="vv-estilo-detalle">Mendoza</span>, en el
+                    marco de la{" "}
+                    <span className="vv-estilo-detalle">
+                      Fiesta Nacional de la Vendimia
+                    </span>
+                    , uno de los eventos más representativos del país y del
+                    trabajo productivo regional. <br />
+                    <br />
+                    Durante su llegada, la titular del Senado evitó profundizar
+                    en tensiones políticas para{" "}
+                    <span className="vv-estilo-detalle">
+                      priorizar el protagonismo de la celebración mendocina
+                    </span>
+                    , destacando el valor histórico y económico de la actividad
+                    vitivinícola. En ese sentido, expresó:
+                  </p>
+
+                  <blockquote className="cita">
+                    "Entiendo que es un momento muy importante para los
+                    mendocinos y no me interesa tampoco desviar la atención de
+                    eso porque son 90 años trabajando para crear una marca país,
+                    un producto del cual dependen muchos mendocinos"
+                  </blockquote>
+
+                  <br />
+
+                  <p>
+                    En la misma línea, Villarruel remarcó la necesidad de{" "}
+                    <span className="vv-estilo-detalle">
+                      poner en el centro la producción y el trabajo
+                    </span>{" "}
+                    como pilares del desarrollo regional, subrayando:
+                  </p>
+
+                  <blockquote className="cita">
+                    "Solamente quiero hablar de lo que hoy es lo más importante:
+                    la Fiesta de la Vendimia"
+                  </blockquote>
+
+                  <br />
+
+                  <p>
+                    De este modo, Villarruel buscó{" "}
+                    <span className="vv-estilo-detalle">
+                      cerrar la polémica política y enfocarse en la agenda
+                      institucional
+                    </span>
+                    , acompañando una actividad clave para la economía mendocina
+                    y reafirmando el valor del{" "}
+                    <span className="vv-estilo-detalle">
+                      federalismo productivo
+                    </span>
+                    .
+                  </p>
+                </>
+              ),
+              imagenes: [mendozaVV2VV, mendozaVV2VVDos, mendozaVV2VVTres],
+            },
+            {
+              titulo: "Recorrida por fincas productivas",
+              descripcion: (
+                <>
+                  <p>
+                    Luego, la vicepresidente realizó una{" "}
+                    <span className="vv-estilo-detalle">
+                      recorrida por distintas fincas productivas
+                    </span>{" "}
+                    donde se cultivan alimentos clave para las economías
+                    regionales como orégano, cebolla, papa, espárragos,
+                    zanahoria y ajo. <br />
+                    <br />
+                    Durante la jornada, compartió encuentros con las familias{" "}
+                    <span className="vv-estilo-detalle">
+                      Moronta, Testa y Castillo
+                    </span>
+                    , quienes le detallaron los{" "}
+                    <span className="vv-estilo-detalle">
+                      procesos de producción, el esfuerzo detrás de cada cosecha
+                    </span>{" "}
+                    y las dificultades que actualmente enfrenta el sector. En
+                    ese contexto, la vicepresidente destacó:
+                  </p>
+
+                  <blockquote className="cita">
+                    "Durante la visita me contaron en detalle sobre los procesos
+                    de producción, el esfuerzo que implica cada cosecha y
+                    también las preocupaciones que hoy atraviesa el sector"
+                  </blockquote>
+
+                  <br />
+
+                  <p>
+                    La actividad permitió visibilizar de primera mano la
+                    realidad de quienes sostienen la producción en el interior
+                    del país, reafirmando la importancia de{" "}
+                    <span className="vv-estilo-detalle">
+                      acompañar al campo y fortalecer las economías regionales
+                    </span>
+                    . En ese sentido, Villarruel expresó:
+                  </p>
+
+                  <blockquote className="cita">
+                    "En cada cultivo y en cada familia que trabaja la tierra, se
+                    ve con claridad algo muy profundo, acá está la Argentina que
+                    trabaja, la que produce, que se esfuerza todos los días y
+                    que sigue apostando al futuro"
+                  </blockquote>
+
+                  <br />
+
+                  <p>
+                    De este modo, la vicepresidente volvió a poner el foco en{" "}
+                    <span className="vv-estilo-detalle">
+                      la cultura del trabajo y la producción
+                    </span>{" "}
+                    como ejes del desarrollo nacional, destacando el rol
+                    fundamental de las familias que sostienen el entramado
+                    productivo del país.
+                  </p>
+                </>
+              ),
+              imagenes: [
+                mendozaVV2VVDiez,
+                mendozaVV2VVOnce,
+                mendozaVV2VVDoce,
+                mendozaVV2VVTrece,
+                mendozaVV2VVCatorce,
+                mendozaVV2VVQuince,
+                mendozaVV2VVDieciseis,
+                mendozaVV2VVDiecisiete,
+                mendozaVV2VVDieciocho,
+                mendozaVV2VVDiecinueve,
+                mendozaVV2VVVeinte,
+              ],
+            },
+            {
+              titulo: "Basílica de San Francisco",
+              descripcion: (
+                <>
+                  <p>
+                    En la misma jornada Victoria Villarruel recorrió la{" "}
+                    <span className="vv-estilo-detalle">
+                      Basílica de San Francisco
+                    </span>
+                    , un sitio de profundo valor histórico y espiritual para la
+                    Argentina. <br />
+                    <br />
+                    Allí descansan los restos de la hija del{" "}
+                    <span className="vv-estilo-detalle">
+                      General José de San Martín
+                    </span>
+                    , y se conserva el legado vinculado a la{" "}
+                    <span className="vv-estilo-detalle">Virgen del Carmen</span>
+                    , a quien el prócer nombró{" "}
+                    <span className="vv-estilo-detalle">
+                      Generala del Ejército de los Andes
+                    </span>
+                    , encomendando bajo su protección la gesta libertadora. En
+                    ese contexto, la vicepresidente expresó:
+                  </p>
+
+                  <blockquote className="cita">
+                    "Visité la Basílica de San Francisco donde yacen los restos
+                    de la hija del General San Martín y que bajo la advocación
+                    de la Virgen del Carmen nuestro prócer la nombró Generala
+                    del Ejército"
+                  </blockquote>
+
+                  <br />
+
+                  <p>
+                    Durante la visita, Villarruel también pudo observar el{" "}
+                    <span className="vv-estilo-detalle">
+                      bastón de mando del Libertador
+                    </span>{" "}
+                    y fue recibida por la{" "}
+                    <span className="vv-estilo-detalle">
+                      Cofradía de la Virgen del Carmen
+                    </span>
+                    , quienes le hicieron entrega de un{" "}
+                    <span className="vv-estilo-detalle">escapulario</span>
+                    , símbolo de fe y tradición. <br />
+                    <br />
+                    La actividad formó parte de un recorrido que integró{" "}
+                    <span className="vv-estilo-detalle">
+                      historia, identidad nacional y espiritualidad
+                    </span>
+                    , resaltando los valores que marcaron el origen de la
+                    Nación.
+                  </p>
+                </>
+              ),
+              imagenes: [
+                mendozaVV2VVCuatro,
+                mendozaVV2VVCinco,
+                mendozaVV2VVSeis,
+                mendozaVV2VVSiete,
+                mendozaVV2VVOcho,
+                mendozaVV2VVNueve,
+              ],
+            },
+          ],
         },
       ],
     },
